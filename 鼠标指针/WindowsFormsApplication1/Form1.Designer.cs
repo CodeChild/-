518d5a2973da01d01c0e23c38052ef3d3463f280
zhangyulong <zhangyulong1997@hotmail.com>
2015-08-19 18:15:27 +0800

    windows10鼠标移动到左上角触发taskview

---
+++ 鼠标指针/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace 边角触发
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.开ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(111, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "x";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(151, 83);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(100, 25);
+            this.textBoxX.TabIndex = 1;
+            this.textBoxX.TextChanged += new System.EventHandler(this.textBoxX_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(111, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "y";
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(151, 146);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(100, 25);
+            this.textBoxY.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "鼠标指针";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.退出ToolStripMenuItem,
+            this.退出ToolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(182, 84);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.开ToolStripMenuItem,
+            this.关ToolStripMenuItem});
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.退出ToolStripMenuItem.Text = "功能开关";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // 开ToolStripMenuItem
+            // 
+            this.开ToolStripMenuItem.Name = "开ToolStripMenuItem";
+            this.开ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.开ToolStripMenuItem.Text = "开  Alt+F1";
+            this.开ToolStripMenuItem.Click += new System.EventHandler(this.开ToolStripMenuItem_Click);
+            // 
+            // 关ToolStripMenuItem
+            // 
+            this.关ToolStripMenuItem.Name = "关ToolStripMenuItem";
+            this.关ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.关ToolStripMenuItem.Text = "关 Alt+F2";
+            this.关ToolStripMenuItem.Click += new System.EventHandler(this.关ToolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem1
+            // 
+            this.退出ToolStripMenuItem1.Name = "退出ToolStripMenuItem1";
+            this.退出ToolStripMenuItem1.Size = new System.Drawing.Size(181, 26);
+            this.退出ToolStripMenuItem1.Text = "退出";
+            this.退出ToolStripMenuItem1.Click += new System.EventHandler(this.退出ToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 282);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "鼠标指针";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 开ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem1;
+    }
+}
+
---
+++ 鼠标指针/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace 边角触发
+{
+
+    public partial class Form1 : Form
+    {
+        bool b = true;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            SetVisibleCore(false);
+            this.Hide();
+            //注册热键Shift+S，Id号为100。HotKey.KeyModifiers.Shift也可以直接使用数字4来表示。  
+            HotKey.RegisterHotKey(Handle, 101, HotKey.KeyModifiers.Alt, Keys.F2);
+            HotKey.RegisterHotKey(Handle, 102, HotKey.KeyModifiers.Alt, Keys.F1);
+            this.notifyIcon1.ShowBalloonTip(100, "边角触发", "边角触发已开启，按alt+f2关闭",ToolTipIcon.Info);
+        }
+
+        private void textBoxX_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (b)
+            {
+                Point pointToScreen = Control.MousePosition;
+                int x = pointToScreen.X, y = pointToScreen.Y;
+                textBoxX.Text = x.ToString();
+                textBoxY.Text = y.ToString();
+                if (x == 0 & y == 0)
+                {
+
+                    keybd_event(0x5b, 0, 0, 0);
+                    keybd_event(9, 0, 0, 0);
+                    keybd_event(0x5b, 0, 2, 0);
+                    keybd_event(9, 0, 2, 0);
+                    Thread.Sleep(1000);
+                }
+
+            }
+        }
+        [DllImport("user32.dll", EntryPoint = "keybd_event")]
+
+        public static extern void keybd_event(
+
+            byte bVk, //虚拟键值
+
+            byte bScan,// 一般为0
+
+            int dwFlags, //这里是整数类型 0 为按下，2为释放
+
+            int dwExtraInfo //这里是整数类型 一般情况下设成为 0
+
+  );
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            SetVisibleCore(true);
+            this.Show();
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void 开ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send("%{F1}");
+        }
+
+        private void 关ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send("%{F2}");
+        }
+
+        private void 退出ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_HOTKEY = 0x0312;
+            //按快捷键  
+            switch (m.Msg)
+            {
+                case WM_HOTKEY:
+                    switch (m.WParam.ToInt32())
+                    {
+                        case 101:     //按下的是Alt+F2
+                           //此处填写快捷键响应代码  
+                            b = false;
+                            this.notifyIcon1.ShowBalloonTip(100, "边角触发", "边角触发已关闭，按alt+f1开启", ToolTipIcon.Info);
+                            break;
+                        case 102:     //按下的是Alt+F1  
+                            //此处填写快捷键响应代码  
+                            b = true;
+                            this.notifyIcon1.ShowBalloonTip(100, "边角触发", "边角触发已开启，按alt+f2关闭", ToolTipIcon.Info);
+                            break;
+                       
+                    }
+                    break;
+            }
+            base.WndProc(ref m);
+        }
+        protected override void SetVisibleCore(bool values)
+        {
+            base.SetVisibleCore(values);
+        }
+
+
+
+    }
+}
